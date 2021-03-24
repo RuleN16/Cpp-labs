6cbc1ac06acf7bbe3d3f9a722481cad3f1dc0a9e
RuleN16 <by.nice.patrick@gmail.com>
2021-03-24 14:41:26 +0600

    Update lab3ex1.cpp

--- lab3ex1.cpp
+++ lab3ex1.cpp
@@ -20,31 +20,30 @@ int main()
 	double a1 = cin_with_number_check();
 	double a2 = cin_with_number_check();
 	double a3 = cin_with_number_check();
-
-    double modulus = sqrt(pow(a1, 2) + pow(a2, 2) + pow(a3, 2));
+	
+	double modulus = sqrt(pow(a1, 2) + pow(a2, 2) + pow(a3, 2));
     
-    cout << "The modulus of the vector is: " << modulus << ".";
+	cout << "The modulus of the vector is: " << modulus << ".";
 
-   return 0;
+   	return 0;
 }
 
 double cin_with_number_check()
 {
-    int check;
-    double component;
+	int check;
+	double component;
+	
+	do {
+        	cout << "Enter the component: ";
+        	cin >> component;
+        	if (cin.fail()) {
+            		cout << "Input error: you must enter the number.\n";
+            		check = 0;
+            		cin.clear();
+            		cin.ignore(32767, '\n');
+            		fflush(stdin);
+        	} else check = 1;		
+    	} while (check == 0);
 
-    do {
-        cout << "Enter the component: ";
-        cin >> component;
-        if (cin.fail()) {
-            cout << "Input error: you must enter the number.\n";
-            check = 0;
-            cin.clear();
-            cin.ignore(32767, '\n');
-            fflush(stdin);
-        }
-        else check = 1;
-    } while (check == 0);
-
-    return component;
+    	return component;
 }
